daece8d9948c6a803ebe5e9d638c2a5eb7b0b79f
Balazs Ujhazi <balazs_ujhazi@epam.com>
2021-05-06 15:53:45 +0200

    Added a test when room doesn't exist

--- ticket-service/src/test/java/com/epam/training/ticketservice/command/CreateScreeningCommandTest.java
+++ ticket-service/src/test/java/com/epam/training/ticketservice/command/CreateScreeningCommandTest.java
@@ -1,10 +1,14 @@
 package com.epam.training.ticketservice.command;
 
+import com.epam.training.ticketservice.entity.Movie;
 import com.epam.training.ticketservice.repository.MovieRepository;
 import com.epam.training.ticketservice.repository.RoomRepository;
 import com.epam.training.ticketservice.repository.ScreeningRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
@@ -14,22 +18,28 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
+@ExtendWith(MockitoExtension.class)
 class CreateScreeningCommandTest {
 
     public static final String MOVIE_NAME = "movieName";
     public static final String ROOM_NAME = "roomName";
     public static final String START_DATE = "startDateString";
 
+    @Mock
+    MovieRepository movieRepository;
+
+    @Mock
+    RoomRepository roomRepository;
+
+    @Mock
+    ScreeningRepository screeningRepository;
+
+    CreateScreeningCommand underTest = new CreateScreeningCommand(movieRepository, roomRepository, screeningRepository);
+
     @Test
     public void testExecuteShouldReturnMovieDoesntExistWhenMovieNotFound() {
         // Given
-        MovieRepository movieRepository = Mockito.mock(MovieRepository.class);
-        RoomRepository roomRepository = Mockito.mock(RoomRepository.class);
-        ScreeningRepository screeningRepository = Mockito.mock(ScreeningRepository.class);
-
         given(movieRepository.findById(MOVIE_NAME)).willReturn(Optional.empty());
-
-        CreateScreeningCommand underTest = new CreateScreeningCommand(movieRepository, roomRepository, screeningRepository);
 
         // When
         String result = underTest.execute(MOVIE_NAME, ROOM_NAME, START_DATE);
@@ -38,6 +48,21 @@ class CreateScreeningCommandTest {
         assertThat(result, equalTo("Movie doesn't exist"));
         verifyNoInteractions(roomRepository, screeningRepository);
         verify(movieRepository).findById(MOVIE_NAME);
+    }
+
+    @Test
+    public void testExecuteShouldReturnRoomDoesntExistWhenRoomNotFound() {
+        // Given
+        Movie movie = Mockito.mock(Movie.class);
+        given(movieRepository.findById(MOVIE_NAME)).willReturn(Optional.of(movie));
+        given(roomRepository.findById(ROOM_NAME)).willReturn(Optional.empty());
+
+        // When
+        String result = underTest.execute(MOVIE_NAME, ROOM_NAME, START_DATE);
+
+        // Then
+        assertThat(result, equalTo("Room doesn't exist"));
+        verifyNoInteractions(screeningRepository);
     }
 
 }
